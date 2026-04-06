e95aa8a3eb67091e105dcfc7de4dcb663adddcad
Tobi Ajila <Tobi_Ajila@ca.ibm.com>
2026-04-06 07:53:56 -0400

    Revert "Add stackoverflow checks to class file parsing"

--- runtime/bcutil/defineclass.c
+++ runtime/bcutil/defineclass.c
@@ -76,7 +76,7 @@ preloadSuperClass(J9VMThread* vmThread, U_8* classData, UDATA classDataLength, J
 			superClassName = J9UTF8_DATA(superClassNameUTF8);
 		}
 	} else {
-		I_32 readResult = j9bcutil_readSuperClassFromClassFileBytes(privatePortLibrary, classData, classDataLength, &superClassNameLength, &buffer, sizeof(buf), vmThread->currentOSStackFree);
+		I_32 readResult = j9bcutil_readSuperClassFromClassFileBytes(privatePortLibrary, classData, classDataLength, &superClassNameLength, &buffer, sizeof(buf));
 		superClassName = buffer;
 
 		if (readResult != 0) {
@@ -809,8 +809,7 @@ callDynamicLoader(J9VMThread *vmThread, J9LoadROMClassData *loadData, U_8 * inte
 				translationFlags,
 				FALSE,
 				TRUE /* isIntermediateROMClass */,
-				localBuffer,
-				vmThread->currentOSStackFree);
+				localBuffer);
 
 			if (BCT_ERR_NO_ERROR == rc) {
 				intermediateData = (U_8 *)intermediateLoadData.romClass;
@@ -836,8 +835,7 @@ callDynamicLoader(J9VMThread *vmThread, J9LoadROMClassData *loadData, U_8 * inte
 			translationFlags,
 			classFileBytesReplacedByRIA | classFileBytesReplacedByRCA,
 			FALSE, /* isIntermediateROMClass */
-			localBuffer,
-			vmThread->currentOSStackFree);
+			localBuffer);
 
 	if (BCT_ERR_NO_ERROR == result) {
 		/* The module of a class transformed by a JVMTI agent needs access to unnamed modules */
@@ -879,8 +877,7 @@ callDynamicLoader(J9VMThread *vmThread, J9LoadROMClassData *loadData, U_8 * inte
 						translationFlags,
 						classFileBytesReplacedByRIA | classFileBytesReplacedByRCA,
 						FALSE, /* isIntermediateROMClass */
-						localBuffer,
-						vmThread->currentOSStackFree);
+						localBuffer);
 
 				j9mem_free_memory(classFileBytes);
 			}
